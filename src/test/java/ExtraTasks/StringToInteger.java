57103d85c5d774b1be69c6c51d3ccf6e30043abd
AsudEsma <asesozdemir@gmail.com>
2022-02-05 22:40:53 +0300

    StringToInteger class is here.

---
+++ src/test/java/ExtraTasks/StringToInteger.java
@@ -0,0 +1,11 @@
+package ExtraTasks;
+
+public class StringToInteger {
+    public static void main(String[] args) {
+        System.out.println((StringToInt("6") + 11));
+    }
+
+    private static int StringToInt(String s) {
+        return Integer.parseInt(s);
+    }
+}
